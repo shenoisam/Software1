0181a1cc1e9faca0babcaa491d2352dbc517922d
Sam Shenoi <Samuel_Shenoi1@baylor.edu>
2020-03-05 14:36:08 -0600

    Testing hooks

--- code/pre-push.sh
+++ code/pre-push.sh
@@ -1,21 +1,22 @@
-#!/bin/bash 
-
 # Author: Sam Shenoi 
-# Description: This program defines a pre-push hook that runs all of the previously made JUnit tests in order to ensure that everything works. 
-# This program calls each test in the test directory in order to accomplish this
+# Description: This program runs the pre-push hook which runs a suite of tests before a push can be committed. 
 
 
-# To skip this and push anyway despite failing tests, run the following command 
+# To escape this and push anyway, please run this command
 # git commit --no-verify -m <Your message> 
 
-# For now since there are no tests, we will use the test script in the test folder 
-./src/test/testscript.sh
 
 
-# If the last value returned an error, we arent ready to push 
+# For now lets run a sample script in the test folder 
+
+./src/test/testscript.sh 
+
+
+# Check to see if the last result worked properly
+
 if [ $? -ne 0 ] 
-then
-       echo "Test failed, please check before commit" 
-       exit 1 
+then 
+	echo "Test failed, please fix before pushing"
+        exit 1 
 fi 
 
