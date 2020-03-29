ad50c590641bf16fdce3982299a1ac00196d15b1
Alejandro_Navarro1 <Alejandro_Navarro1@baylor.edu>
2020-03-29 14:15:00 -0500

    fixed merge confilt

--- code/src/main/java/frontend/Runner.java
+++ code/src/main/java/frontend/Runner.java
@@ -2,13 +2,9 @@ package frontend;
 
 import java.awt.BorderLayout;
 import java.awt.Container;
-<<<<<<< HEAD
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
-=======
->>>>>>> Jordan
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
