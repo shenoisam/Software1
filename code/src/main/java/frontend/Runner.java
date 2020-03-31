f384ad761737a9e7e5382b8bf54c7e7bfbd2cbb3
Sam Shenoi <Samuel_Shenoi1@baylor.edu>
2020-03-30 23:19:47 -0500

    Fixed katies code

--- code/src/main/java/frontend/Runner.java
+++ code/src/main/java/frontend/Runner.java
@@ -11,6 +11,7 @@ import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextArea;
