ba3cb4cf1a1f36171bb228190479a934c1dd4eee
shenoisam <nikhilshenoi@nikhils-iMac.attlocal.net>
2020-04-20 15:53:54 -0500

    Added generic query for Diagnosis

--- code/src/main/java/backend/classes/Diagnosis.java
+++ code/src/main/java/backend/classes/Diagnosis.java
@@ -1,11 +1,20 @@
 package backend.classes;
 
+import java.util.List;
+
+import backend.NotImplementedException;
+
 public class Diagnosis {
 	private String Name; 
 	private String Description; 
 
 	public Diagnosis() {
 		// TODO Auto-generated constructor stub
+	}
+
+	public Diagnosis(List<String> headerRow, List<Object> list) {
+		// TODO Auto-generated constructor stub
+		throws new NotImplementedException(); 
 	}
 
 	public String getName() {
--- code/src/main/java/backend/classes/User.java
+++ code/src/main/java/backend/classes/User.java
@@ -45,7 +45,7 @@ public class User {
 
 
 	public GenericRunner accept(EHRRunner ehrRunner) {
-		// TODO Auto-generated method stub
+		return null; 
 		
 	}
 	
