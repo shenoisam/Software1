845b65174bee26903c7bfdb93d83a00a1c0e6f07
Alejandro_Navarro1 <Alejandro_Navarro1@baylor.edu>
2020-04-28 09:37:22 -0500

    changes

--- code/src/main/java/frontend/provider/ProviderFrontend.java
+++ code/src/main/java/frontend/provider/ProviderFrontend.java
@@ -188,10 +188,7 @@ public class ProviderFrontend implements IProviderFrontend{
 	      // adding the top Panel to the contianer that was passed in to the function
 	      pane.add(topPanel, BorderLayout.NORTH);
 	   }
-<<<<<<< HEAD
-	@Override
-=======
->>>>>>> master
+
 	public void createAndShowGUI(JFrame frame) {
 		// TODO Auto-generated method stub
 		
