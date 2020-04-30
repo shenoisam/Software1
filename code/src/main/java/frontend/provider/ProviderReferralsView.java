dcf87ac861aba46114bc76f57928ddfb7c53dc60
Jordan Hurt <jordan_hurt@baylor.edu>
2020-04-30 02:00:19 -0500

    adding export to referrals

--- code/src/main/java/frontend/provider/ProviderReferralsView.java
+++ code/src/main/java/frontend/provider/ProviderReferralsView.java
@@ -7,6 +7,12 @@ import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,72 +21,81 @@ import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.ScrollPaneConstants;
 
-
 import backend.classes.*;
 import businesslayer.CShareObjects;
 
-public class ProviderReferralsView extends ProviderFrontend{
-	
-   // Have a list of doctors that we can update based on specility 
-   List<Doctor> docs; 
-   private Patient pat; 
+public class ProviderReferralsView extends ProviderFrontend {
+
+   // Have a list of doctors that we can update based on specility
+   List<Doctor> docs;
+   private Patient pat;
+   private String selectedDoctor;
+   private String referralReason;
+   private String addedNotes;
+   private List<PatientDiagnosis> diags;
+
    public ProviderReferralsView(ProviderRunner p) {
-		super(p);
-		docs = new ArrayList<Doctor>(); 
-	}
+      super(p);
+      docs = new ArrayList<Doctor>();
+   }
 
-public ProviderReferralsView(ProviderRunner providerRunner, Patient pat) {
-	// TODO Auto-generated constructor stub
-	this(providerRunner);
-	this.pat = pat; 
-	
-}
+   public ProviderReferralsView(ProviderRunner providerRunner, Patient pat) {
+      // TODO Auto-generated constructor stub
+      this(providerRunner);
+      this.pat = pat;
 
-public void patientReferralPanel(Container pane) {
+   }
+
+   public void patientReferralPanel(Container pane) {
       // creating the main referral panel
       JPanel referralPanel = new JPanel();
-      referralPanel.setLayout(new GridLayout(1,2));
-      
-      // creating the left hand side of the page 
+      referralPanel.setLayout(new GridLayout(1, 2));
+
+      // creating the left hand side of the page
       JPanel leftPanel = new JPanel();
       leftPanel.setLayout(new BoxLayout(leftPanel, BoxLayout.Y_AXIS));
-      
+
       // creating the specality section
       JPanel specialtyPanel = new JPanel();
       specialtyPanel.setBorder(BorderFactory.createTitledBorder("Specialty"));
-      JComboBox specalites = new JComboBox(new Object[] { "", "Radiology", "Obstetrics & Gynecology", "Cardiovascular", "Anesthesiology", "Orthopaedic Surgery", "Ophthalmology", "Dermatology", "Pediatrics" });
-      
+      JComboBox specalites = new JComboBox(new Object[] { "", "Radiology", "Obstetrics & Gynecology", "Cardiovascular",
+            "Anesthesiology", "Orthopaedic Surgery", "Ophthalmology", "Dermatology", "Pediatrics" });
+
       specialtyPanel.add(specalites);
       leftPanel.add(specialtyPanel);
-      
+
       // creating the provider section
       JPanel providerPanel = new JPanel();
       providerPanel.setBorder(BorderFactory.createTitledBorder("Provider"));
-      
+
       /**** Data Retrieve *****/
-      String [] fields = {};
-      String [] params = {};
+      String[] fields = {};
+      String[] params = {};
       docs = serv.getData(CShareObjects.DOCTOR, fields, params);
-      
-      
+
       JComboBox providers = new JComboBox(docs.toArray());
       providerPanel.add(providers);
-      
+
+      selectedDoctor = providers.getSelectedItem().toString();
+
       // adding the provider panel to the left panel
       leftPanel.add(providerPanel);
-      
+
       // creating the additional notes section
       JPanel notes = new JPanel();
       notes.setBorder(BorderFactory.createTitledBorder("Additional Notes"));
       notes.setLayout(new BoxLayout(notes, BoxLayout.Y_AXIS));
-      
+
       // creating the notes entered text area
       JTextArea notesEntered = new JTextArea(4, 10);
       notesEntered.setText("");
@@ -90,28 +105,30 @@ public void patientReferralPanel(Container pane) {
       scroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
       notes.add(scroll);
       
+      addedNotes = notesEntered.getText().toString();
+
       // adding the notes to the notes and submit section
       leftPanel.add(notes);
-      
+
       // adding the left panel to the main panel veiw
       referralPanel.add(leftPanel);
-      
-      // creating the right hand side of the page 
+
+      // creating the right hand side of the page
       JPanel rightPanel = new JPanel();
-      rightPanel.setLayout(new GridLayout(2,1));
-      
+      rightPanel.setLayout(new GridLayout(2, 1));
+
       // creating reason for referral panel
       JPanel reason = new JPanel();
       reason.setBorder(BorderFactory.createTitledBorder("Reason For Referral"));
       reason.setLayout(new BoxLayout(reason, BoxLayout.Y_AXIS));
-      
+
       /***** Data retrieval ******/
-      String [] fields2 = {"PatientID"};
-      String [] params2 = {pat.getID()};
-      List<PatientDiagnosis> diags = serv.getData(CShareObjects.PATIENTDIAGNOSIS, fields2, params2);
-      
+      String[] fields2 = { "PatientID" };
+      String[] params2 = { pat.getID() };
+      diags = serv.getData(CShareObjects.PATIENTDIAGNOSIS, fields2, params2);
+
       for (PatientDiagnosis pd : diags) {
-    	  reason.add(createDiagnosis(pd));
+         reason.add(createDiagnosis(pd));
       }
       JCheckBox other = new JCheckBox("Other (input reason below)");
       other.setSelected(false);
@@ -119,7 +136,7 @@ public void patientReferralPanel(Container pane) {
       JTextArea otherReason = new JTextArea(1, 5);
       otherReason.setEditable(true);
       reason.add(otherReason);
-      
+
       // adding invisible panels for formating
       reason.add(new JPanel());
       reason.add(new JPanel());
@@ -128,30 +145,60 @@ public void patientReferralPanel(Container pane) {
 
       // adding the reason for referral to the right panel
       rightPanel.add(reason);
-      
+
       // creating the refer button
       JPanel refferButtonPanel = new JPanel();
       refferButtonPanel.setLayout(new BoxLayout(refferButtonPanel, BoxLayout.LINE_AXIS));
       refferButtonPanel.add(new JPanel());
-      refferButtonPanel.add(new JButton("Refer"));
+
+      // creating the new referral button
+      JButton refer = new JButton("Refer");
+
+      refer.addActionListener(new ActionListener() {
+         public void actionPerformed(ActionEvent e) {
+            JFileChooser fileChooser = new JFileChooser();
+            fileChooser.setCurrentDirectory(new File(System.getProperty("user.dir")));
+            JPanel parent = new JPanel();
+            int result = fileChooser.showSaveDialog(parent);
+            File save = fileChooser.getSelectedFile();
+            Writer writer = null;
+
+            try {
+               // open the file
+               // write to the file
+               // close the file
+               writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(save), "utf-8"));
+               
+               writer.write(referalLetter());
+               
+               writer.close();
+               
+            } catch (IOException e1) {
+               // TODO Auto-generated catch block
+               e1.printStackTrace();
+            } 
+         }
+      });
+
+      refferButtonPanel.add(refer);
       refferButtonPanel.add(new JPanel());
-      
+
       // adding the submit panel to the notes and submit panel
       rightPanel.add(refferButtonPanel);
 
       // adding the right panel to the main referral page
       referralPanel.add(rightPanel);
-      
+
       // adding the referral panel to the passed in container
       pane.add(referralPanel);
    }
 
    private JCheckBox createDiagnosis(PatientDiagnosis d) {
-	   JCheckBox daignosis1 = new JCheckBox(d.getName());
-	   daignosis1.setSelected(false);
-	   return daignosis1;
+      JCheckBox daignosis1 = new JCheckBox(d.getName());
+      daignosis1.setSelected(false);
+      return daignosis1;
    }
-   
+
    public void createAndShowGUI(JFrame frame) {
       // creating the panes within the screen
       providerSideBar(frame.getContentPane(), pat);
@@ -159,13 +206,24 @@ public void patientReferralPanel(Container pane) {
       patientReferralPanel(frame.getContentPane());
 
    }
-   
+
    public void createAndShowGUI(JFrame frame, Patient pat) {
-	   this.pat = pat; 
-	   createAndShowGUI(frame);
+      this.pat = pat;
+      createAndShowGUI(frame);
    }
+
+   private String referalLetter() {
+      String letter = "Dear Dr. " + selectedDoctor + ",\n\n\t" + pat.getFullName() + " (DOB: " + pat.getDOB() + "), "
+            + pat.getGender() + ", was recently evaluated in our office. " + pat.getFirstName()
+            + "currently has a diagnosis of " + diags.toString() + "\n\tWe are reffering this patient for "
+            + referralReason + ". We have discussed possible treatment options should " + pat.getFirstName()
+            + " require treatment. \n\nI look forward to working with you directly in the treatment of Patient Name’s. "
+            + "Please do not hesitate to contact me directly with any questions "
+            + "or comments you may have concerning thier care. " + "\n\nSincerely,\n\tDr." + p.getUser()
+            +"\n\nAdditional Notes: " + addedNotes + "\n";
+
+      return letter;
+
+   }
+
 }
-
-
-
-
