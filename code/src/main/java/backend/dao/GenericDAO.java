1c2988e259eb46e5a07b46ebc87463f2e1d4316f
kt <kjwokoek@gmail.com>
2020-04-29 23:05:34 -0500

    started factory use in generic not done

--- code/src/main/java/backend/dao/GenericDAO.java
+++ code/src/main/java/backend/dao/GenericDAO.java
@@ -12,6 +12,8 @@ import java.sql.ResultSetMetaData;
 import backend.NotImplementedException;
 import backend.SQLConnection.SQLConnectionPool;
 import backend.SQLConnection.SQLConnectionPoolFactory;
+import backend.classes.Perscription;
+import backend.factory.FactoryObjects;
 
 /**
  * Defines a generic Database Access Object (DAO) specific for connecting to a table; 
@@ -79,6 +81,26 @@ public abstract class GenericDAO {
         return data; 
 		
 		
+	}
+	/*private List<Perscription> generateList(List<List<Object>> stuff) {
+		 List<Perscription> finalList = new ArrayList<Perscription>(); 
+		 List<String> headerRow = listToString(stuff.get(0));
+		 for(int i = 1; i < stuff.size(); i++) {
+			 //TODO: implement this; 
+			 finalList.add(new Perscription(headerRow, stuff.get(i)));
+		 }
+		 
+		 return finalList;
+	}*/
+	protected <T>List<T> generateList(List<List<Object>> stuff, FactoryObjects<T> object) {
+		 List<T> finalList = new ArrayList<T>(); 
+		 List<String> headerRow = listToString(stuff.get(0));
+		 for(int i = 1; i < stuff.size(); i++) {
+			 //TODO: implement this; 
+			 finalList.add(object.generateObject(headerRow, stuff.get(i)));
+		 }
+		 
+		 return finalList;
 	}
 
 	protected void update(String table, String [] fields,String rmStr,String [] params) throws SQLException {
